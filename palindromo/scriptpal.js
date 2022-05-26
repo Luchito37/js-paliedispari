1dd31e6e81fd60103112acb37e6f2ad7f4805b2b
Luchito37 <lucamatteotti01@gmail.com>
2022-05-26 15:25:27 +0200

    quasi finito esercizio numero due

---
+++ palindromo/scriptpal.js
@@ -0,0 +1,19 @@
+/*
+Palindroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+*/
+const richiestaParola = prompt("inserire una parola : ")
+let arrayPrimo = []
+
+    arrayPrimo.push(richiestaParola)
+
+    console.log(arrayPrimo)
+
+
+
+
+
+
+
+
